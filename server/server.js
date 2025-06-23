3ba1c7df7a9f720f7050e207f854c22554c596ac
David Nafisy <davidnafisy3@gmail.com>
2025-06-23 11:16:09 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const absenRoutes = require('./absenRoutes');
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
 app.use('/api/absen', absenRoutes);
