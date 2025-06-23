e30a7e0b8113d024516a6be2f0fb1ac791b982bd
David Nafisy <davidnafisy3@gmail.com>
2025-06-23 11:08:50 +0700

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'absensi_db',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'absensi_db',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
